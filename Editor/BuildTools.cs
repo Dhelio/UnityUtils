37b4f255e4d52c46009def9e0f36387ff8c8ddd1
Dhelio <Dhelio@users.noreply.github.com>
2023-08-31 11:59:53 +0200

    Update BuildTools.cs

--- Editor/BuildTools.cs
+++ Editor/BuildTools.cs
@@ -10,25 +10,43 @@ using System.Threading.Tasks;
 
 namespace CastrimarisStudios.Core.Editor {
 
+    /// <summary>
+    /// Miscellaneous build tools
+    /// </summary>
     public class BuildTools {
 
         #region PRIVATE VARIABLES
 
         private const string TAG = nameof(BuildTools);
-        private const string baseFileName = "BuildBaseName";
+        private const string baseFileName = "Fincons_Metaverse_";
         private const string standaloneFolderName = "LinuxStandalone";
         private const string mobileAppendName = "Mobile.apk";
         private const string questAppendName = "Quest.apk";
         private const string linuxServerAppendName = "Server";
-        private static readonly string[] scenes = { "" }; //TODO automate scene finding 
 
         #endregion
 
         #region PRIVATE METHODS
 
+        private static string[] FindEnabledScenes() {
+            var selectedScenes = (from scene in EditorBuildSettings.scenes
+                                  where scene.enabled
+                                  select scene.path).ToArray();
+            return selectedScenes;
+        }
+
         private static void CheckBuildTarget(NamedBuildTarget namedBuildTarget, BuildTarget buildTarget) {
             if (EditorUserBuildSettings.activeBuildTarget != buildTarget) {
                 var result = EditorUserBuildSettings.SwitchActiveBuildTarget(namedBuildTarget, buildTarget);
+                if (!result) {
+                    Debug.LogError($"[{TAG}] - Error while trying to switch active build target!");
+                }
+            }
+        }
+
+        private static void CheckBuildTarget(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget) {
+            if (EditorUserBuildSettings.activeBuildTarget != buildTarget) {
+                var result = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
                 if (!result) {
                     Debug.LogError($"[{TAG}] - Error while trying to switch active build target!");
                 }
@@ -52,7 +70,7 @@ namespace CastrimarisStudios.Core.Editor {
         }
 
         private static void MakeOculusBuild(string Path) {
-            CheckBuildTarget(NamedBuildTarget.Android, BuildTarget.Android);
+            CheckBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 
             Debug.Log($"[{TAG}] - Setting up Oculus build...");
             EnableOculusPlugin(true);
@@ -64,12 +82,12 @@ namespace CastrimarisStudios.Core.Editor {
                 targetGroup = BuildTargetGroup.Android,
                 target = BuildTarget.Android,
                 locationPathName = locationPathName,
-                scenes = scenes
+                scenes = FindEnabledScenes()
             });
         }
 
         private static void MakeAndroidBuild(string Path) {
-            CheckBuildTarget(NamedBuildTarget.Android, BuildTarget.Android);
+            CheckBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 
             Debug.Log($"[{TAG}] - Setting up Android build...");
             EnableOculusPlugin(false);
@@ -81,23 +99,25 @@ namespace CastrimarisStudios.Core.Editor {
                 targetGroup = BuildTargetGroup.Android,
                 target = BuildTarget.Android,
                 locationPathName = locationPathName,
-                scenes = scenes
+                scenes = FindEnabledScenes()
             });
         }
 
         private static void MakeLinuxServerBuild(string Path, bool ShouldEnableOculusPlugin = true) {
-            CheckBuildTarget(NamedBuildTarget.Server, BuildTarget.StandaloneLinux64);
+            EditorUserBuildSettings.standaloneBuildSubtarget = StandaloneBuildSubtarget.Server;
+            CheckBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64);
 
             Debug.Log($"[{TAG}] - Setting up Linux Server build...");
             EnableOculusPlugin(ShouldEnableOculusPlugin);
 
             var locationPathName = System.IO.Path.Combine(Path, standaloneFolderName, baseFileName +linuxServerAppendName);
 
-            var report = BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
-                targetGroup = BuildTargetGroup.LinuxHeadlessSimulation,
-                target = BuildTarget.LinuxHeadlessSimulation,
-                locationPathName = locationPathName,
-                scenes = scenes
+    var report = BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
+        subtarget = (int)StandaloneBuildSubtarget.Server,
+        targetGroup = BuildTargetGroup.Standalone,
+        target = BuildTarget.StandaloneLinux64,
+        locationPathName = locationPathName,
+        scenes = FindEnabledScenes()
             });
         }
 
@@ -106,7 +126,7 @@ namespace CastrimarisStudios.Core.Editor {
         #region PUBLIC METHODS
 
 
-        [MenuItem("Tools/Build Android and Quest")]
+        [MenuItem("Tools/Build/Build for Android and Quest")]
         public static void BuildForAndroidPhoneAndQuest() {
             string path = EditorUtility.SaveFolderPanel("Choose APKs location", "", baseFileName);
 
@@ -116,17 +136,39 @@ namespace CastrimarisStudios.Core.Editor {
             MakeAndroidBuild(path);
         }
 
-        [MenuItem("Tools/Build Android, Quest and Linux Server")]
+
+
+        [MenuItem("Tools/Build/Build for Android, Quest and Linux Server")]
         public static void BuildAndroidAndLinuxStandalone() {
             string path = EditorUtility.SaveFolderPanel("Choose builds save location.",Directory.GetCurrentDirectory(), "");
 
             Debug.Log($"[{TAG}] - Saving player builds to {path}...");
 
+            VersioningEditor.SkipBuild = true;
+            VersioningEditor.UpdateBuildNumber();
             MakeOculusBuild(path);
             MakeAndroidBuild(path);
             MakeLinuxServerBuild(path);
+            VersioningEditor.SkipBuild = false;
         }
 
+        [MenuItem("Tools/Switch Build Target/Linux")]
+        public static void SwitchToLinux() {
+            EditorUserBuildSettings.standaloneBuildSubtarget = StandaloneBuildSubtarget.Server;
+            CheckBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64);
+        }
+
+        [MenuItem("Tools/Switch Build Target/Android")]
+        public static void SwitchToAndroid() {
+            EnableOculusPlugin(false);
+            CheckBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        }
+
+        [MenuItem("Tools/Switch Build Target/Oculus")]
+        public static void SwitchToOculus() {
+            EnableOculusPlugin(true);
+            CheckBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        }
         #endregion
 
     }
