017a6419139711f99f73f0e4a2aa732caedf9fed
Dhelio <Dhelio@users.noreply.github.com>
2023-09-04 18:48:02 +0200

    Update TransformExtensions.cs

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -45,5 +45,50 @@ namespace Castrimaris.Core {
             return result.ToArray();
         }
 
+
+        /// <summary>
+        /// Destroys all childs of this transform immediately. You are strogly recommended to use DestroyChilds instead.
+        /// </summary>
+        /// <returns>The number of destroyed childs.</returns>
+        public static int DestroyChildsImmediate(this Transform target) {
+            int i;
+            for (i = 0; target.childCount > 0; i++) {
+                GameObject.DestroyImmediate(target.GetChild(0).gameObject);
+            }
+            return i+1;
+        }
+
+        /// <summary>
+        /// Destroys all childs of this transform.
+        /// </summary>
+        /// <returns>The number of destroyed childs.</returns>
+        public static int DestroyChilds(this Transform target) {
+            int i;
+            for (i = 0; i < target.childCount; i++) {
+                GameObject.Destroy(target.GetChild(i).gameObject);
+            }
+            return i+1;
+        }
+
+        /// <summary>
+        /// Sets all children of this transform to the indicated active state.
+        /// </summary>
+        /// <param name="ChildrenActiveState">True if all childs should be active, false otherwise</param>
+        public static void SetChildrenActiveState(this Transform target, bool ChildrenActiveState) {
+            for (int i = 0; i < target.childCount; i++) {
+                target.GetChild(i).gameObject.SetActive(ChildrenActiveState);
+            }
+        }
+
+        /// <summary>
+        /// Shuffles children order randomly.
+        /// </summary>
+        public static void ShuffleChildren(this Transform target) {
+            System.Random rng = new System.Random();
+            for (int i = 0; i < target.childCount; i++) {
+                target.GetChild(i).SetSiblingIndex(rng.Next(0, target.childCount - 1));
+            }
+        }
+        
     }
 }
